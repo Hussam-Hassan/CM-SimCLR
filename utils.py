81b4b6e57e708985e4b83252cc38c68b82a7cab7
Hussam <56823164+Hussam-Hassan@users.noreply.github.com>
2020-12-11 05:59:17 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,35 +1,90 @@
 from PIL import Image
-from torchvision import transforms
 from torchvision.datasets import CIFAR10
-import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import torch
+import torchvision
 import random
-
+import os
+import os.path
+import numpy as np
+import pickle
+from typing import Any, Callable, Optional, Tuple
 
 class CIFAR10Pair(CIFAR10):
     """CIFAR10 Dataset.
     """
 
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-        img = Image.fromarray(img)
+    def __init__(
+            self,
+            root: str,
+            train: bool = True,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            download: bool = False,
+    ) -> None:
 
-        
+        super(CIFAR10, self).__init__(root, transform=transform,
+                                      target_transform=target_transform)
+
+        self.train = train  # training set or test set
+
+        if download:
+            self.download()
+
+        if not self._check_integrity():
+            raise RuntimeError('Dataset not found or corrupted.' +
+                               ' You can use download=True to download it')
+
+        if self.train:
+            downloaded_list = self.train_list
+        else:
+            downloaded_list = self.test_list
+
+        self.data: Any = []
+        self.targets = []
+
+        # now load the picked numpy arrays
+        for file_name, checksum in downloaded_list:
+            file_path = os.path.join(self.root, self.base_folder, file_name)
+            with open(file_path, 'rb') as f:
+                entry = pickle.load(f, encoding='latin1')
+                self.data.append(entry['data'])
+                if 'labels' in entry:
+                    self.targets.extend(entry['labels'])
+                else:
+                    self.targets.extend(entry['fine_labels'])
+
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
+
+        self._load_meta()
         self.class_idxs = dict()  # Construct a dictionary of lists of indices for each class.
         targets1 = np.unique(self.targets)
         for target1 in targets1:
             idx = np.where(self.targets == target1)[0].tolist()
             self.class_idxs[target1] = idx
 
-        class_idx = self.class_idxs[target]
-        idx = random.choice(class_idx)
-        paired_img = self.data[idx]
-        paired_img = Image.fromarray(paired_img)
 
 
-        
+    def __getitem__(self, index, match_class=True, match_prob=.9):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
         if self.transform is not None:
             pos_1 = self.transform(img)
-            pos_2 = self.transform(paired_img)
+
+            u = random.uniform(0, 1)
+
+            if match_class and match_prob >= u:
+                class_idx = self.class_idxs[target]
+                idx = random.choice(class_idx)
+                paired_img = self.data[idx]
+                paired_img = Image.fromarray(paired_img)
+                pos_2 = self.transform(paired_img)
+            else:
+                pos_2 = self.transform(img)
 
         if self.target_transform is not None:
             target = self.target_transform(target)
